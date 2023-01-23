8b9ba05b0fcb69e1d67ee208a6fb2b53ae084955
yewon <ajdanddl6321@gmail.com>
2023-01-23 20:03:49 +0900

    2023.01.23 알고리즘 공부

---
+++ java/src/programmers/이진수_더하기.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class 이진수_더하기 {
+
+	class Solution {
+	    public String solution(String bin1, String bin2) {
+	        String answer = Integer.toBinaryString(Integer.parseInt(bin1, 2)+Integer.parseInt(bin2, 2));
+	        return answer;
+	    }
+	}
+
+}
